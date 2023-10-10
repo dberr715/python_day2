5ea5c9a21f0adfb223331acd0c4d2832c4d3676e
David Berry <dberry3755@gmail.com>
2023-10-10 15:06:54 -0400

    uncommented things

--- loops.py
+++ loops.py
@@ -1,7 +1,7 @@
 # part 1 Start/End
 
-# start_num = int(input("Enter positive starting number: "))
-# end_num = int(input("Enter a positive ending number: "))
+start_num = int(input("Enter positive starting number: "))
+end_num = int(input("Enter a positive ending number: "))
 
 # ORIGINAL ANSWER
 # while start_num <= end_num:
@@ -10,14 +10,14 @@
 
 # part 1 bonus
 
-# while start_num <= end_num:
-#     if start_num < 1 or start_num > 50 or end_num > 50 or end_num < 1:
-#         print("Pick a number 1-50")
-#         start_num = int(input("Enter positive starting number: "))
-#         end_num = int(input("Enter a positive ending number: "))
-#     else:
-#         print(start_num)
-#         start_num += 5
+while start_num <= end_num:
+    if start_num < 1 or start_num > 50 or end_num > 50 or end_num < 1:
+        print("Pick a number 1-50")
+        start_num = int(input("Enter positive starting number: "))
+        end_num = int(input("Enter a positive ending number: "))
+    else:
+        print(start_num)
+        start_num += 5
 
 
 # part 2 Multi-Condition Check
